66795f3bc66580e63891f1667f3fa3561887c929
Ke <mezereonxp@gmail.com>
2018-09-19 21:21:40 +0800

    [UPDATE] Add university class

---
+++ src/main/java/org/spring/springboot/dao/UniversityMapper.java
@@ -0,0 +1,31 @@
+package org.spring.springboot.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.spring.springboot.domain.University;
+import org.spring.springboot.domain.UniversityExample;
+
+public interface UniversityMapper {
+    long countByExample(UniversityExample example);
+
+    int deleteByExample(UniversityExample example);
+
+    int deleteByPrimaryKey(Integer universityId);
+
+    int insert(University record);
+
+    int insertSelective(University record);
+
+    List<University> selectByExample(UniversityExample example);
+
+    University selectByPrimaryKey(Integer universityId);
+
+    int updateByExampleSelective(@Param("record") University record, @Param("example") UniversityExample example);
+
+    int updateByExample(@Param("record") University record, @Param("example") UniversityExample example);
+
+    int updateByPrimaryKeySelective(University record);
+
+    int updateByPrimaryKey(University record);
+}
---
+++ src/main/java/org/spring/springboot/domain/University.java
@@ -0,0 +1,23 @@
+package org.spring.springboot.domain;
+
+public class University {
+    private Integer universityId;
+
+    private String universityName;
+
+    public Integer getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(Integer universityId) {
+        this.universityId = universityId;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName == null ? null : universityName.trim();
+    }
+}
