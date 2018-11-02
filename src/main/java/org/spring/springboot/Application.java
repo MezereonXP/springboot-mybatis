970cea9eaeee8a52930b6133fe22cfdd73fc2bf8
MezereonXP <mezereonxp@gmail.com>
2018-11-02 14:39:15 +0800

    [UPDATE] Add encryption of password

--- src/main/java/org/spring/springboot/Application.java
+++ src/main/java/org/spring/springboot/Application.java
@@ -1,11 +1,13 @@
 package org.spring.springboot;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.spring.springboot.dao.CityDao;
 import org.spring.springboot.domain.City;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * Spring Boot 应用启动类
@@ -14,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 // Spring Boot 应用的标识
 @SpringBootApplication
+@EnableEncryptableProperties
 // mapper 接口类扫描包配置
 @MapperScan("org.spring.springboot")
 public class Application {
